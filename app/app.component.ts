8483a92793aef0dfa64dbf17c7c77f29f37671f7
Samuel Murillo <samuelmurillo333@gmail.com>
2017-01-11 19:26:04 -0400

    Add Side Drawer button. New colors

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,24 @@
-import { Component, AfterViewInit ,ViewChild, ChangeDetectorRef } from "@angular/core";
+import { Component, AfterViewInit, ViewChild, ChangeDetectorRef } from "@angular/core";
 import { RadSideDrawerComponent, SideDrawerType } from "nativescript-telerik-ui/sidedrawer/angular";
+import { TNSFontIconService } from 'nativescript-ng2-fonticon';
 
 @Component({
     selector: "my-app",
     templateUrl: "./app.component.html"
 })
-export class AppComponent implements AfterViewInit{
+export class AppComponent implements AfterViewInit {
     @ViewChild(RadSideDrawerComponent)
     public drawerComponent: RadSideDrawerComponent;
     private drawer: SideDrawerType;
-    public pages:Array<Object>;
-    private mainContentText = "lel";
+    public pages: Array<Object>;
 
-    constructor (
-        private _changeDetectionRef: ChangeDetectorRef) {
+    constructor(private _changeDetectionRef: ChangeDetectorRef,
+                private fonticon: TNSFontIconService) {
         // List all pages here
         this.pages = [
-            {name:"Home"},
-            {name:"About"},
-            {name:"Contact"}
+            { name: "Home" },
+            { name: "About" },
+            { name: "Contact" }
         ];
     }
 
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 import { SIDEDRAWER_DIRECTIVES } from "nativescript-telerik-ui/sidedrawer/angular";
+import { TNSFontIconModule } from 'nativescript-ng2-fonticon';
 
 import { AppComponent } from "./app.component";
 
@@ -9,6 +10,9 @@ import { AppComponent } from "./app.component";
     imports: [
         NativeScriptModule,
         NativeScriptFormsModule,
+        TNSFontIconModule.forRoot({
+            'mdi': 'material-design-icons.css'
+        })
     ],
     declarations: [
         SIDEDRAWER_DIRECTIVES,
