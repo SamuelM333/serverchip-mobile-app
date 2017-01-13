e34ab60e882124d9d4e1cb975eb7c82abe6a8ead
Samuel Murillo <samuelmurillo333@gmail.com>
2017-01-13 13:34:13 -0400

    Add card list on microchips component

--- app/microchips/microchips.component.ts
+++ app/microchips/microchips.component.ts
@@ -3,10 +3,29 @@ import { TNSFontIconService } from 'nativescript-ng2-fonticon';
 
 @Component({
     selector: "microchips",
-    templateUrl: "./microchips/microchips.component.html"
+    templateUrl: "./microchips/microchips.component.html",
+    styleUrls: ["./microchips/microchips.component.css"]
 })
 export class MicrochipsComponent {
 
-    constructor() {}
+    public items: Array<any>;
+
+    constructor() {
+        this.items = [
+            { name: "kek1", continent: "wut" },
+            { name: "kek2", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+        ];
+
+    }
+
+    onTap(args) {
+        console.log(args.index);
+        console.log(this.items[args.index].name);
+    }
 
 }
