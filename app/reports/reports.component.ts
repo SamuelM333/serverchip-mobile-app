6ad32f4ce2249f949710d2e99d89d04c6eb994b5
Samuel Murillo <samuelmurillo333@gmail.com>
2017-01-13 13:26:42 -0400

    Add card list on reports

--- app/reports/reports.component.ts
+++ app/reports/reports.component.ts
@@ -4,10 +4,34 @@ import { TNSFontIconService } from 'nativescript-ng2-fonticon';
 
 @Component({
     selector: "reports",
-    templateUrl: "./reports/reports.component.html"
+    templateUrl: "./reports/reports.component.html",
+    styleUrls: ["./reports/reports.component.css"]
 })
 export class ReportsComponent {
 
-    constructor() {}
+    public items: Array<any>;
+
+    constructor() {
+        this.items = [
+            { name: "kek1", continent: "wut" },
+            { name: "kek2", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+            { name: "kek3", continent: "wut" },
+        ];
+        // const app = require('application');
+        //
+        // if (app.android) {
+        //     app.android.on(app.AndroidApplication.activityBackPressedEvent, () => this.onBack());
+        // }
+    }
+
+    onTap(args) {
+        console.log(args.index);
+        console.log(this.items[args.index].name);
+    }
+
 
 }
